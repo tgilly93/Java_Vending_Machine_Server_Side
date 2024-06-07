58ee9297e2fb265efc230f90c0c50910469087cc
markevis williams <markeviswilliams@gmail.com>
2024-06-07 10:42:24 -0400

    MW-checking in

--- src/main/java/com/techelevator/model/Bank.java
+++ src/main/java/com/techelevator/model/Bank.java
@@ -5,19 +5,24 @@ import java.math.BigDecimal;
 
 public class Bank {
     private BigDecimal totalMoney;
-    private BigDecimal totalAmountInserted;
+
+    private Money moneyInserted;
+    private Money bankTotal;    private BigDecimal totalAmountInserted;
     public Bank() {
         totalMoney = BigDecimal.valueOf(0.00);
         totalAmountInserted = BigDecimal.valueOf(0.00);
+
     }
     public void addCoin(Money money) {
         totalMoney = totalMoney.add(money.getValue());
         totalAmountInserted = totalAmountInserted.add(money.getValue());
     }
-    public BigDecimal getTotalMoney() {
-        return totalMoney;
+    public Money getTotalMoney() {
+        this.bankTotal = new Money(totalMoney);
+        return bankTotal;
     }
-    public BigDecimal getTotalAmountInserted() {
-        return totalAmountInserted;
+    public Money getTotalAmountInserted() {
+        this.moneyInserted = new Money(totalAmountInserted);
+        return moneyInserted;
     }
 }
